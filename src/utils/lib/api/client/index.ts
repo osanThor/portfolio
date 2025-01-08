6a1f3960d3bad80ca37189dc7edf574b585e671f
junyoung <jadw9715@gmail.com>
2025-01-09 00:28:59 +0900

    Doc. Given's Log Ver.2 추가

--- src/utils/lib/api/client/index.ts
+++ src/utils/lib/api/client/index.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 const client = axios.create();
 client.defaults.withCredentials = true;
 client.defaults.baseURL =
-  process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3001";
+  process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 
 export default client;
